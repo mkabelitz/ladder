09a7e72464e04a304806ea3ac4ba880a807d599b
Marco Kabelitz <marco.kabelitz@rwth-aachen.de>
2017-08-27 15:41:14 +0200

    Added stuff

--- train.py
+++ train.py
@@ -28,7 +28,7 @@ FLAGS = flags.FLAGS
 flags.DEFINE_integer('num_labeled', 4000, 'Number of labeled samples to use for training. (None = all labeled samples)')
 flags.DEFINE_integer('batch_size', 100, 'Number of samples used per batch.')
 flags.DEFINE_integer('num_iters', 50000, 'Number of training steps.')
-flags.DEFINE_integer('eval_interval', 500, 'Number of steps between evaluations.')
+flags.DEFINE_integer('eval_interval', 200, 'Number of steps between evaluations.')
 flags.DEFINE_float('learning_rate', 0.001, 'Initial learning rate for optimizer.')
 flags.DEFINE_float('lr_decay_steps', 12500, 'Interval of steps for learning rate decay.')
 flags.DEFINE_float('lr_decay_factor', 0.1, 'Learning rate exponential decay factor.')
@@ -49,18 +49,19 @@ def main(_):
                                                           capacity=FLAGS.batch_size * 100,
                                                           min_after_dequeue=FLAGS.batch_size * 20)
     data_te_batch, labels_te_batch = u.load_batch(data_te, labels_te, FLAGS.batch_size)
+    data_unlabeled_batch, _ = u.load_batch(unlabeled, labels_tr, FLAGS.batch_size)
 
     with tf.variable_scope('model') as scope:
         model = models.get_model(FLAGS.model_name)
         logits_tr, _, _ = model(data_tr_batch, is_training=True)
         scope.reuse_variables()
-        # _, crt, cln = model(data_unlabeled_batch, is_training=True)
+        _, crt, cln = model(data_unlabeled_batch, is_training=True)
         logits_te, _, _ = model(data_te_batch, is_training=False)
 
     loss_tr = u.get_supervised_loss(logits=logits_tr, labels=labels_tr_batch)
     loss_te = u.get_supervised_loss(logits=logits_te, labels=labels_te_batch)
 
-    # loss_tr += u.get_gamma_loss(crt, cln, 4.0)
+    loss_tr += u.get_gamma_loss(crt, cln, 4.0)
 
     acc_tr = u.get_accuracy(logits_tr, labels_tr_batch)
     acc_te = u.get_accuracy(logits_te, labels_te_batch)
