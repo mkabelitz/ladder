f8a963d48a59367a64afa5ad64afe77ec31e0164
Marco Kabelitz <marco.kabelitz@rwth-aachen.de>
2017-08-27 14:58:02 +0200

    Added stuff

--- train.py
+++ train.py
@@ -49,10 +49,7 @@ def main(_):
                                                           capacity=FLAGS.batch_size * 100,
                                                           min_after_dequeue=FLAGS.batch_size * 20)
     data_te_batch, labels_te_batch = u.load_batch(data_te, labels_te, FLAGS.batch_size)
-    data_unlabeled_batch = u.load_unlabeled_shuffle_batch(unlabeled,
-                                                          batch_size=FLAGS.batch_size,
-                                                          capacity=FLAGS.batch_size * 100,
-                                                          min_after_dequeue=FLAGS.batch_size * 20)
+    data_unlabeled_batch = u.load_unlabeled_batch(unlabeled, FLAGS.batch_size)
 
     with tf.variable_scope('model') as scope:
         model = models.get_model(FLAGS.model_name)
