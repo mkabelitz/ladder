907ae45ca939e0e26c165a1dffedbece70e900e6
Marco Kabelitz <marco.kabelitz@rwth-aachen.de>
2017-09-07 02:53:58 +0200

    Added stuff

--- cifar10_50k_supervised_rasmus.py
+++ cifar10_50k_supervised_rasmus.py
@@ -19,7 +19,7 @@ FLAGS = flags.FLAGS
 flags.DEFINE_integer('num_labeled', None, 'Number of labeled samples to use for training. (None = all labeled samples)')
 flags.DEFINE_integer('batch_size', 100, 'Number of samples used per batch.')
 flags.DEFINE_integer('num_iters', 50000, 'Number of training steps.')
-flags.DEFINE_integer('eval_interval', 600, 'Number of steps between evaluations.')
+flags.DEFINE_integer('eval_interval', 500, 'Number of steps between evaluations.')
 flags.DEFINE_float('learning_rate', 0.001, 'Initial learning rate for optimizer.')
 flags.DEFINE_float('lr_decay_steps', 12500, 'Interval of steps for learning rate decay.')
 flags.DEFINE_float('lr_decay_factor', 0.1, 'Learning rate exponential decay factor.')
