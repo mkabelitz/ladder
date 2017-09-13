fbfd934460924c9ca958181782df4e5e491bf00c
Marco Kabelitz <marco.kabelitz@rwth-aachen.de>
2017-09-13 15:30:45 +0200

    Added stuff

--- models.py
+++ models.py
@@ -29,8 +29,8 @@ def _gamma_layer(data, activation_fn, is_training, noise_std, batch_norm_decay, 
             # normalized_enc = (data - mean_enc) / tf.sqrt(var_enc + 1e-10)
             normalized_enc = (data - ema.average(running_mean_enc)) / tf.sqrt(ema.average(running_var_enc) + 1e-10)
     else:
-        normalized_enc = (data - mean_enc) / tf.sqrt(var_enc + 1e-10)
-        # normalized_enc = (data - ema.average(running_mean_enc)) / tf.sqrt(ema.average(running_var_enc) + 1e-10)
+        # normalized_enc = (data - mean_enc) / tf.sqrt(var_enc + 1e-10)
+        normalized_enc = (data - ema.average(running_mean_enc)) / tf.sqrt(ema.average(running_var_enc) + 1e-10)
 
     z_tilde = _noise(normalized_enc, noise_std)
     with tf.variable_scope('bn_correct'):
@@ -55,8 +55,8 @@ def _gamma_layer(data, activation_fn, is_training, noise_std, batch_norm_decay, 
             # normalized_dec = (h_tilde - mean_dec) / tf.sqrt(var_dec + 1e-10)
             normalized_dec = (h_tilde - ema.average(running_mean_dec)) / tf.sqrt(ema.average(running_var_dec) + 1e-10)
     else:
-        normalized_dec = (h_tilde - mean_dec) / tf.sqrt(var_dec + 1e-10)
-        # normalized_dec = (h_tilde - ema.average(running_mean_dec)) / tf.sqrt(ema.average(running_var_dec) + 1e-10)
+        # normalized_dec = (h_tilde - mean_dec) / tf.sqrt(var_dec + 1e-10)
+        normalized_dec = (h_tilde - ema.average(running_mean_dec)) / tf.sqrt(ema.average(running_var_dec) + 1e-10)
 
     z_est = _g(z_tilde, normalized_dec)
 
