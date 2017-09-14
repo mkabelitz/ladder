b892b78aa106de835bc15bf66796b2a95b177baa
Marco Kabelitz <marco.kabelitz@rwth-aachen.de>
2017-09-14 19:10:21 +0200

    Added stuff

--- mnist_100_gamma.py
+++ mnist_100_gamma.py
@@ -19,7 +19,7 @@ FLAGS = flags.FLAGS
 
 flags.DEFINE_integer('num_labeled', 100, 'Number of labeled samples to use for training. (None = all labeled samples)')
 flags.DEFINE_integer('batch_size', 100, 'Number of samples used per batch.')
-flags.DEFINE_integer('num_iters', 12000, 'Number of training steps.')
+flags.DEFINE_integer('num_iters', 5000, 'Number of training steps.')
 flags.DEFINE_integer('eval_interval', 500, 'Number of steps between evaluations.')
 flags.DEFINE_float('learning_rate', 0.002, 'Initial learning rate for optimizer.')
 flags.DEFINE_float('decay_first', 0.67, 'Percentage after when to start learning rate decay.')
