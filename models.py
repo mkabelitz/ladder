50142d9d6d1dc15816572f2a30a165c74995bd8a
Marco Kabelitz <marco.kabelitz@rwth-aachen.de>
2017-09-14 18:59:01 +0200

    Added stuff

--- models.py
+++ models.py
@@ -466,13 +466,13 @@ def _gamma_layer(data, activation_fn, is_training, is_unlabeled, noise_std, ema,
         running_var_enc = tf.get_variable('running_var_enc', shape=[data.get_shape()[-1]], trainable=False,
                                           initializer=tf.constant_initializer(1.0))
     mean_enc, var_enc = tf.nn.moments(data, axes=[0])
-    if is_unlabeled and noise_std == 0.0:
+    if is_training:
         assign_mean_enc = running_mean_enc.assign(mean_enc)
         assign_var_enc = running_var_enc.assign(var_enc)
         bn_assigns.append(ema.apply([running_mean_enc, running_var_enc]))
         with tf.control_dependencies([assign_mean_enc, assign_var_enc]):
             normalized_enc = (data - mean_enc) / tf.sqrt(var_enc + 1e-10)
-    elif is_training:
+    elif is_unlabeled:
         normalized_enc = (data - mean_enc) / tf.sqrt(var_enc + 1e-10)
     else:
         normalized_enc = (data - ema.average(running_mean_enc)) / tf.sqrt(ema.average(running_var_enc) + 1e-10)
@@ -493,13 +493,13 @@ def _gamma_layer(data, activation_fn, is_training, is_unlabeled, noise_std, ema,
         running_var_dec = tf.get_variable('running_var_dec', shape=[data.get_shape()[-1]], trainable=False,
                                           initializer=tf.constant_initializer(1.0))
         mean_dec, var_dec = tf.nn.moments(h_tilde, axes=[0])
-    if is_unlabeled and noise_std == 0.0:
+    if is_training:
         assign_mean_dec = running_mean_dec.assign(mean_dec)
         assign_var_dec = running_var_dec.assign(var_dec)
         bn_assigns.append(ema.apply([running_mean_dec, running_var_dec]))
         with tf.control_dependencies([assign_mean_dec, assign_var_dec]):
             normalized_dec = (h_tilde - mean_dec) / tf.sqrt(var_dec + 1e-10)
-    elif is_training:
+    elif is_unlabeled:
         normalized_dec = (h_tilde - mean_dec) / tf.sqrt(var_dec + 1e-10)
     else:
         normalized_dec = (h_tilde - ema.average(running_mean_dec)) / tf.sqrt(ema.average(running_var_dec) + 1e-10)
