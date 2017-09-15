0118ab4070d45db3ae5a8193b5a3b18f8217362b
Marco Kabelitz <marco.kabelitz@rwth-aachen.de>
2017-09-15 11:44:30 +0200

    Added stuff

--- pytorch_test/pytorch_test.py
+++ pytorch_test/pytorch_test.py
@@ -70,11 +70,11 @@ test_loader = torch.utils.data.DataLoader(
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(1, 32, kernel_size=5)
-        self.conv2 = nn.Conv2d(32, 64, kernel_size=3)
-        self.conv3 = nn.Conv2d(64, 64, kernel_size=3)
-        self.conv4 = nn.Conv2d(64, 128, kernel_size=3)
-        self.conv5 = nn.Conv2d(128, 10, kernel_size=3)
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=5, padding=2)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
+        self.conv3 = nn.Conv2d(64, 64, kernel_size=3, padding=1)
+        self.conv4 = nn.Conv2d(64, 128, kernel_size=3, padding=2)
+        self.conv5 = nn.Conv2d(128, 10, kernel_size=3, padding=2)
         self.fc1 = nn.Linear(10, 10)
 
     def forward(self, x):
