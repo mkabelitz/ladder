53219293be356d6b627f1ff7ffdd9f3ba54a8b1e
Marco Kabelitz <marco.kabelitz@rwth-aachen.de>
2017-09-15 18:32:59 +0200

    Added stuff

--- pytorch_mnist.py
+++ pytorch_mnist.py
@@ -115,7 +115,7 @@ class Net(nn.Module):
         x = F.avg_pool2d(x, kernel_size=x.size()[2:])
 
         x = x.view(-1, 10)
-        x = F.relu(self.fc1_bn(self.fc1(x)))
+        x = self.fc1(x)
 
         return F.log_softmax(x)
 
