dfe8bc2c4a51b127e056f2bd653f392882eb428c
Marco Kabelitz <marco.kabelitz@rwth-aachen.de>
2017-09-15 11:37:04 +0200

    Added stuff

--- pytorch_test/pytorch_test.py
+++ pytorch_test/pytorch_test.py
@@ -49,22 +49,46 @@ test_loader = torch.utils.data.DataLoader(
     batch_size=args.test_batch_size, shuffle=True, **kwargs)
 
 
+# class Net(nn.Module):
+#     def __init__(self):
+#         super(Net, self).__init__()
+#         self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
+#         self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+#         self.conv2_drop = nn.Dropout2d()
+#         self.fc1 = nn.Linear(320, 50)
+#         self.fc2 = nn.Linear(50, 10)
+#
+#     def forward(self, x):
+#         x = F.relu(F.max_pool2d(self.conv1(x), 2))
+#         x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
+#         x = x.view(-1, 320)
+#         x = F.relu(self.fc1(x))
+#         x = F.dropout(x, training=self.training)
+#         x = self.fc2(x)
+#         return F.log_softmax(x)
+
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
-        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
-        self.conv2_drop = nn.Dropout2d()
-        self.fc1 = nn.Linear(320, 50)
-        self.fc2 = nn.Linear(50, 10)
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=5)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3)
+        self.conv3 = nn.Conv2d(64, 64, kernel_size=3)
+        self.conv4 = nn.Conv2d(64, 128, kernel_size=3)
+        self.conv5 = nn.Conv2d(128, 10, kernel_size=3)
+        self.fc1 = nn.Linear(10, 10)
 
     def forward(self, x):
-        x = F.relu(F.max_pool2d(self.conv1(x), 2))
-        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
-        x = x.view(-1, 320)
+        x = F.relu(self.conv1(x))
+        x = F.max_pool2d(x, 2, stride=2)
+        x = F.relu(self.conv2(x))
+        x = F.relu(self.conv3(x))
+        x = F.max_pool2d(x, 2, stride=2)
+        x = F.relu(self.conv4(x))
+        x = F.relu(self.conv5(x))
+        print("    !!!!!!!!!!!    HERE:", x.size())
+        x = F.avg_pool2d(x, kernel_size=x.size()[2:])
+        x = x.view(-1, 10)
         x = F.relu(self.fc1(x))
-        x = F.dropout(x, training=self.training)
-        x = self.fc2(x)
         return F.log_softmax(x)
 
 model = Net()
