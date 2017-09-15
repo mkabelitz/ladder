564f8849eda134c9464d5970013ddaf838851d7b
Marco Kabelitz <marco.kabelitz@rwth-aachen.de>
2017-09-15 16:51:36 +0200

    Added stuff

--- pytorch_mnist.py
+++ pytorch_mnist.py
@@ -14,7 +14,7 @@ parser.add_argument('--batch-size', type=int, default=100, metavar='N',
                     help='input batch size for training (default: 100)')
 parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
                     help='input batch size for testing (default: 1000)')
-parser.add_argument('--labeled-samples', type=int, default=100, metavar='N',
+parser.add_argument('--labeled-samples', type=int, default=None, metavar='N',
                     help='number of labeled samples for training, None for all (default: 100)')
 parser.add_argument('--epochs', type=int, default=12000, metavar='N',
                     help='number of epochs to train (default: 20)')
