dae32eb86bfe3dcde092d4010b585ffa88a6974e
Marco Kabelitz <marco.kabelitz@rwth-aachen.de>
2017-09-15 21:59:44 +0200

    Added stuff

--- pytorch_mnist.py
+++ pytorch_mnist.py
@@ -84,7 +84,7 @@ class Net(nn.Module):
 
         self.conv1 = nn.Conv2d(1, 32, kernel_size=5, padding=2)
         self.conv1_bn = nn.BatchNorm2d(num_features=32, affine=False, momentum=args.bn_momentum)
-        self.conv1_bias = nn.Parameter(torch.ones(1))
+        self.conv1_bias = nn.Parameter(torch.ones(self.conv1.out_channels))
 
         self.pool1_bn = nn.BatchNorm2d(num_features=32, affine=False, momentum=args.bn_momentum)
 
