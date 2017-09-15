da7d6e1ad9b8412ff6a554edebb9526c2eaf1feb
Marco Kabelitz <marco.kabelitz@rwth-aachen.de>
2017-09-15 20:50:49 +0200

    Added stuff

--- pytorch_mnist.py
+++ pytorch_mnist.py
@@ -72,9 +72,9 @@ test_loader = torch.utils.data.DataLoader(mnist_te_dataset,
 
 
 class Net(nn.Module):
-    def gaussian(self, ins, stddev=0.45):
+    def gaussian(self, ins, stddev=0.3):
         if self.training:
-            return ins + Variable(torch.randn(ins.size()[2]).cuda() * stddev)
+            return ins + Variable(torch.randn(ins.size()).cuda() * stddev)
         return ins
 
     def __init__(self):
