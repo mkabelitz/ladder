f25621006eb0c31b2ade813158851962ca77e123
Marco Kabelitz <marco.kabelitz@rwth-aachen.de>
2017-09-15 13:30:18 +0200

    Added stuff

--- pytorch_test/pytorch_test.py
+++ pytorch_test/pytorch_test.py
@@ -116,7 +116,7 @@ def test():
         correct += pred.eq(target.data.view_as(pred)).cpu().sum()
 
     test_loss /= len(test_loader.dataset)
-    print('Test set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+    print('Test set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)'.format(
         test_loss, correct, len(test_loader.dataset),
         100. * correct / len(test_loader.dataset)))
 
