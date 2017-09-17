98fdf2e4a362a5608afe0ea11b0534096d8145f0
Marco Kabelitz <marco.kabelitz@rwth-aachen.de>
2017-09-18 01:43:16 +0200

    Added stuff

--- pytorch_mnist.py
+++ pytorch_mnist.py
@@ -82,7 +82,7 @@ class Noise(nn.Module):
             return x
         else:
             self.noise.data.normal_(0, std=self.std)
-            print(x.size(), self.noise.size())
+            # print(x.size(), self.noise.size())
             return x + self.noise
 
 
