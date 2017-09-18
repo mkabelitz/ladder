d22e54399f87650d209505f8bdc3a277dc1a3c58
Marco Kabelitz <marco.kabelitz@rwth-aachen.de>
2017-09-18 03:11:00 +0200

    Added stuff

--- pytorch_mnist.py
+++ pytorch_mnist.py
@@ -248,8 +248,8 @@ def train():
         _, z = model(unlabeled)
         ce_loss = F.nll_loss(softmax, target)
         mse_loss = F.mse_loss(z, z_est)
-        loss = ce_loss + mse_loss
-        # loss = ce_loss
+        # loss = ce_loss + mse_loss
+        loss = mse_loss
         loss.backward()
         optimizer.step()
         if args.train_log_interval and step % args.train_log_interval == 0:
