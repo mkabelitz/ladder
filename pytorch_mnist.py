36daec9b587c9de5a2817264f14edf57a8e22cd5
Marco Kabelitz <marco.kabelitz@rwth-aachen.de>
2017-09-18 14:18:50 +0200

    Added stuff

--- pytorch_mnist.py
+++ pytorch_mnist.py
@@ -246,7 +246,7 @@ def train():
         _, z_est = model(unlabeled)
         Noise.add_noise = False
         _, z = model(unlabeled)
-        print(z.size())
+        print(z.data)
         ce_loss = F.nll_loss(softmax, target)
         mse_loss = F.mse_loss(z, z_est)
         loss = ce_loss + mse_loss
