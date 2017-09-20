25a52a95cedf435374437dcd806d59f5d6f932f6
Marco Kabelitz <marco.kabelitz@rwth-aachen.de>
2017-09-20 13:38:50 +0200

    Added stuff

--- pytorch_mnist.py
+++ pytorch_mnist.py
@@ -263,11 +263,11 @@ def train():
         optimizer.zero_grad()
         softmax_crt, _, _, _ = model(data)
         # model.apply(lambda x: x.eval() if 'BatchNorm' in str(type(x)) else False)
-        _, _, z_est, z_cln = model(unlabeled)
+        # _, _, z_est, z_cln = model(unlabeled)
         ce_loss = F.nll_loss(softmax_crt, target)
-        mse_loss = F.mse_loss(z_cln, z_est)
-        loss = ce_loss + mse_loss
-        # loss = mse_loss
+        # mse_loss = F.mse_loss(z_cln, z_est)
+        # loss = ce_loss + mse_loss
+        loss = ce_loss
         loss.backward()
         optimizer.step()
 
