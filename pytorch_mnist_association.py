c64fd8d338c6adec1595673fca14c7c37642ba71
Marco Kabelitz <marco.kabelitz@rwth-aachen.de>
2017-09-20 19:59:02 +0200

    Added stuff

--- pytorch_mnist_association.py
+++ pytorch_mnist_association.py
@@ -122,7 +122,7 @@ def get_visit_loss(p, weight=1.0):
 
     visit_probability = torch.sum(p, dim=0)
     t_nb = p.size()[1]
-    visit_loss = F.soft_margin_loss(torch.ones((1, t_nb)) / t_nb, torch.log(1e-8 + visit_probability))
+    visit_loss = F.mse_loss(torch.ones((1, t_nb)) / t_nb, torch.log(1e-8 + visit_probability) * weight)
     return visit_loss
 
     # visit_probability = tf.reduce_mean(
