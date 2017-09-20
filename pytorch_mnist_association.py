bd9af44bbe95d4bc9a4607738a225bca469d8715
Marco Kabelitz <marco.kabelitz@rwth-aachen.de>
2017-09-20 17:27:30 +0200

    Added stuff

--- pytorch_mnist_association.py
+++ pytorch_mnist_association.py
@@ -160,7 +160,7 @@ def test():
 # test()
 
 
-a = Variable(torch.FloatTensor([1,2,3,4,5,6,7,8,9,10])).repeat(10, 1)
+a = Variable(torch.FloatTensor([1,2,2,4,1,6,8,8,9,10])).repeat(10, 1)
 print(a)
 b = torch.transpose(a, 0, 1)
 print(b)
