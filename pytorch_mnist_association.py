11037143aef069dce0a1f7ce738db2f788774c7a
Marco Kabelitz <marco.kabelitz@rwth-aachen.de>
2017-09-20 18:24:47 +0200

    Added stuff

--- pytorch_mnist_association.py
+++ pytorch_mnist_association.py
@@ -137,7 +137,7 @@ def get_semisup_loss(a, b, labels, walker_weight=1.0, visit_weight=1.0):
     print(p_ab)
     p_ba = F.log_softmax(torch.transpose(match_ab, 0, 1))
     print(p_ba)
-    p_aba = F.log_softmax(torch.mm(p_ab, p_ba))
+    p_aba = F.softmax(torch.mm(p_ab, p_ba))
     print(p_aba)
 
     loss_aba = F.nll_loss(p_aba, p_target)
