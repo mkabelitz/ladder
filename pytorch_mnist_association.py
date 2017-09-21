3b31c74b6e7fa7e2dbd7892fc6795e08fb486e2f
Marco Kabelitz <marco.kabelitz@rwth-aachen.de>
2017-09-21 15:35:31 +0200

    Added stuff

--- pytorch_mnist_association.py
+++ pytorch_mnist_association.py
@@ -223,7 +223,8 @@ def train():
         softmax = F.log_softmax(logits)
         ce_loss = F.nll_loss(softmax, target)
         loss_aba, visit_loss = get_semisup_loss(emb_l, emb_u, target)
-        loss = ce_loss + (loss_aba + visit_loss)
+        # loss = ce_loss + (loss_aba + visit_loss)
+        loss = torch.sum(emb_l + emb_u)
         loss.backward()
         optimizer.step()
 
